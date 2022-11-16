fb42ee84be9a08e94793c4d0b7cbd934ffffaaf3
author-Ry <ry.wtnb@outlook.com>
2022-11-16 22:23:48 +0000

    directory sort

--- Fundamentals/_01_hello_world.ts
+++ Fundamentals/_01_hello_world.ts
@@ -1,7 +1,7 @@
 console.log("Hello world");
 
 // ディレクトリベース
-// deno run step01/hello_world.ts
+// deno run hello_world.ts
 
 // URLベース
 // deno run https://deno.land/std@0.103.0/examples/welcome.ts
